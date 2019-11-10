5e4c0557c7df0bbfeeed7b95e182ad753c94f7eb
Michelle M Williams <mmwilliamscodes@gmail.com>
2019-11-10 15:51:07 -0800

    added links and picture to landing page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { directive } from '@babel/types';
 
 class Contact extends Component {
     render(){
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
-import { directive } from '@babel/types';
+import { Grid, Cell } from 'react-mdl';
+import Photo from './images/moi2.jpg';
 
 class Landing extends Component {
     render(){
         return(
-            <div><h1>Landing Page</h1></div>
+            <div style ={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src={Photo}
+                            alt="me"
+                            className="avatar-img"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+
+                        <hr/>
+
+                        <p>HTML/CSS | Bootstrap | Javascript | PHP | React | NodeJS | Express | MySQL | MongoDB </p>
+
+                            <div className="social-links">
+                                
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/michelle-williams-3b8a48132/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fab fa-linkedin" aria-hidden="true" />
+                                </a>
+
+                                {/* Github */}
+                                <a href="https://github.com/mmwilliamscodes" target="_blank" rel="noopener noreferrer">
+                                    <i className="fab fa-github" aria-hidden="true" />
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
