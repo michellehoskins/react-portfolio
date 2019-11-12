522d938334179c996d23f069afe002c45e1ceb70
Michelle M Williams <mmwilliamscodes@gmail.com>
2019-11-12 14:08:42 -0800

    contact information added

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import Photo2 from './images/Moi.jpg';
 
 class Contact extends Component {
     render(){
         return(
-            <div><h1>Contact Page</h1></div>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Michelle M Williams</h2>
+                        <img 
+                            src={Photo2}
+                            alt="me"
+                            style={{height: '250px'}}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Web Developer, Software Developer who can work anywhere</p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+
+                        <div className="contact-list">
+                        <List>
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                                    440-666-1120</ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-envelope" aria-hidden="true"/>
+                                    michelle@mmwilliams.com</ListItemContent>
+                            </ListItem>
+                        </List>
+                        </div>
+
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
