afb3c2808c86975d9b65a71d8916fc7dec3a3b0b
Michelle M Williams <mmwilliamscodes@gmail.com>
2019-12-02 17:13:57 -0800

    added 2 projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -18,18 +18,18 @@ class Projects extends Component {
             </h2>
             <div className="projects">
               <div className="item">
-                <a href="#!">
+                <a href="https://mmwilliamscodes.github.io/landing-page/" target="_blank" rel="noopener noreferrer">
                   <img src={Project1} alt="project1" />
                 </a>
-                <a href="#!" className="btn-light"><i className="fas fa-eye"></i>Project</a>
-                <a href="#!" className="btn-dark"><i className="fab fa-github"></i>Github</a>
+                <a href="https://mmwilliamscodes.github.io/landing-page/" target="_blank" rel="noopener noreferrer" className="btn-light"><i className="fas fa-eye"></i>Landing Page</a>
+                <a href="https://github.com/mmwilliamscodes/landing-page" target="_blank" rel="noopener noreferrer" className="btn-dark"><i className="fab fa-github"></i>Github</a>
               </div>
               <div className="item">
-                <a href="#!">
+                <a href="https://mmwilliamscodes.github.io/social_network_page/" target="_blank" rel="noopener noreferrer">
                   <img src={Project2} alt="project2" />
                 </a>
-                <a href="#!" className="btn-light"><i className="fas fa-eye"></i>Project</a>
-                <a href="#!" className="btn-dark"><i className="fab fa-github"></i>Github</a>
+                <a href="https://mmwilliamscodes.github.io/social_network_page/" target="_blank" rel="noopener noreferrer" className="btn-light"><i className="fas fa-eye"></i>Social Network</a>
+                <a href="https://github.com/mmwilliamscodes/social_network_page" target="_blank" rel="noopener noreferrer" className="btn-dark"><i className="fab fa-github"></i>Github</a>
               </div>
               <div className="item">
                 <a href="#!">
